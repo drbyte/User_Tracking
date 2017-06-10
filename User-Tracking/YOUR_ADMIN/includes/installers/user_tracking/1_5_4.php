91b36d26fbbefc5f1aad29d62fd83b863a886e61
mc12345678 <mc12345678@users.noreply.github.com>
2017-06-10 03:03:27 -0400

    Add version installer file for v1.5.4.

---
+++ User-Tracking/YOUR_ADMIN/includes/installers/user_tracking/1_5_4.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package functions
+ * @copyright Copyright 2003-2017 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: mc12345678 thanks to bislewl 6/9/2015
+ */
+
+$zc150 = (PROJECT_VERSION_MAJOR > 1 || (PROJECT_VERSION_MAJOR == 1 && substr(PROJECT_VERSION_MINOR, 0, 3) >= 5));
+if ($zc150) { // continue Zen Cart 1.5.0
+    // Updated installer to restore proper operation.
+    
+} // END OF VERSION 1.5.x INSTALL
