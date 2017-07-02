20aafc2144daf46e1ea7a336d8b5e7b2f0f79cc3
mc12345678 <mc12345678@users.noreply.github.com>
2017-07-02 18:57:36 -0400

    Add a few language related changes to the additional language
    There had been some changes made to english that were not
    carried over to the farsi folder (even though the farsi
    folder still contains english...

---
+++ User-Tracking/YOUR_ADMIN/includes/languages/farsi/extra_definitions/user_tracking.php
@@ -0,0 +1,22 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+//  $Id: usertracking 2017-06-30 mc12345678 http://mc12345678.com
+define('BOX_TOOLS_USER_TRACKING', 'User Tracking');
+define('BOX_TOOLS_USER_TRACKING_CONFIG', 'User Tracking Config');
