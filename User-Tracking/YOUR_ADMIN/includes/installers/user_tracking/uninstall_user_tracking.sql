33baff3a46394036bc19e8b20e3de876271f6e5d
mc12345678 <mc12345678@users.noreply.github.com>
2017-07-16 15:57:08 -0400

    Update uninstall script to prevent removal of configuration_group_id = 0

--- User-Tracking/YOUR_ADMIN/includes/installers/user_tracking/uninstall_user_tracking.sql
+++ User-Tracking/YOUR_ADMIN/includes/installers/user_tracking/uninstall_user_tracking.sql
@@ -6,12 +6,10 @@ DROP TABLE IF EXISTS user_tracking;
 SELECT @UserTrackgID := configuration_group_id 
 FROM configuration_group where configuration_group_title LIKE '%User Tracking%';
 
-DELETE FROM configuration WHERE configuration_group_id = @UserTrackgID; 
-
 DELETE FROM admin_pages WHERE page_key = 'UserTracking';
 
 DELETE FROM admin_pages WHERE page_key = 'UserTrackingConfig';
 
-DELETE FROM configuration WHERE configuration_group_id = @UserTrackgID; 
+DELETE FROM configuration WHERE configuration_group_id = @UserTrackgID AND @UserTrackgID != 0;
 
-DELETE FROM configuration_group WHERE configuration_group_id = @UserTrackgID;
+DELETE FROM configuration_group WHERE configuration_group_id = @UserTrackgID AND @UserTrackgID != 0;
